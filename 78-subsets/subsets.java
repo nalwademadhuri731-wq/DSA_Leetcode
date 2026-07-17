82b436ca130148eac63baca44ae691f0c91f8eb5
Madhuri Nalwade <nalwademadhuri731@gmail.com>
2026-07-17 22:36:49 +0530

    Time: 1 ms (86.89%) | Memory: 43.9 MB (95.20%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -2,15 +2,16 @@ class Solution {
     //new approach of power set 
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>>ans=new ArrayList<>();
-        
-        for(int i=0;i<Math.pow(2,nums.length);i++){
-             List<Integer>list=new ArrayList<>();
-             for(int j=0;j<nums.length;j++){
-                if(((1<<j)&i)!=0){
-                    list.add(nums[j]);
-                }
+        ans.add(new ArrayList<>());
+
+        for(int num:nums){
+             int n=ans.size();
+             for(int j=0;j<n;j++){
+               List<Integer>temp=new ArrayList<>(ans.get(j));
+               temp.add(num);
+               ans.add(temp);
              }
-             ans.add(list);
+             
         }
 
      return ans;
