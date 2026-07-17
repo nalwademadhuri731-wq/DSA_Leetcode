989354845e675b64c560be0d9f28386471302592
Madhuri Nalwade <nalwademadhuri731@gmail.com>
2026-07-17 22:20:29 +0530

    Time: 1 ms (86.89%) | Memory: 44.1 MB (75.79%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,27 +1,18 @@
 class Solution {
-
-
+    //new approach of power set 
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>>ans=new ArrayList<>();
-        List<Integer>list=new ArrayList<>();
-
-        tofind(0,ans,list,nums);
-        return ans;
-    }
-    //TO find the subsets :-Recurssion problem 
-    public void  tofind(int i,List<List<Integer>>ans,
-    List<Integer>list,int [] nums){
-        if(i==nums.length){
-            ans.add(new ArrayList<>(list));
-            return;
+        
+        for(int i=0;i<Math.pow(2,nums.length);i++){
+             List<Integer>list=new ArrayList<>();
+             for(int j=0;j<nums.length;j++){
+                if(((1<<j)&i)!=0){
+                    list.add(nums[j]);
+                }
+             }
+             ans.add(list);
         }
-        list.add(nums[i]);
-        tofind(i+1,ans,list,nums);
 
-        list.remove(list.size()-1);
-        tofind(i+1,ans,list,nums);
-
-
+     return ans;
     }
-    
 }
