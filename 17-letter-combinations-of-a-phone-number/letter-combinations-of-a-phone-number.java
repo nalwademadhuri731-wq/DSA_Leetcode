955875b37dab870e0b9bd9b9595318ad97b66406
Madhuri Nalwade <nalwademadhuri731@gmail.com>
2026-07-21 18:27:30 +0530

    Time: 3 ms (30.84%) | Memory: 48.8 MB (21.64%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -1,32 +1,36 @@
 class Solution {
-    public void tofind(int j,List<String> list,String digits, HashMap<Character,String>map,String str){
+    public void tofind(int j,int k,List<String> list,String digits, String [] map,String str){
         if(str.length()==digits.length()){
            String  s=str;
            list.add(s);
            return;
         }
-        String demo=map.get(digits.charAt(j));
+        String demo=map[digits.charAt(k)-'0'];
 
         for(int i=0;i<demo.length();i++){
-            
-            tofind(j+1,list,digits,map,str+demo.charAt(i));
+           
+            tofind(j+1,k+1,list,digits,map,str+demo.charAt(i));
         }
 
     }
     public List<String> letterCombinations(String digits) {
           List<String> list=new ArrayList<>();
-        int j=0;
-        HashMap<Character,String>map=new HashMap<>();
-        map.put('2',"abc");
-        map.put('3',"def");
-        map.put('4',"ghi");
-        map.put('5',"jkl");
-        map.put('6',"mno");
-        map.put('7',"pqrs");
-        map.put('8',"tuv");
-        map.put('9',"wxyz");
+        int j=digits.charAt(0)-'0';
+        // HashMap<Character,String>map=new HashMap<>();
+        // map.put('2',"abc");
+        // map.put('3',"def");
+        // map.put('4',"ghi");
+        // map.put('5',"jkl");
+        // map.put('6',"mno");
+        // map.put('7',"pqrs");
+        // map.put('8',"tuv");
+        // map.put('9',"wxyz");
+        int k=0;
+        String [] map={
+            "","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"
+        };
         String str="";
-        tofind(j,list,digits,map,str);
+        tofind(j,k,list,digits,map,str);
         return list;
     }
 }
